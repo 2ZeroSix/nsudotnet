be20b90005b75369d7020114dd48cfac2b440a73
2zerosix <2zerosix1@gmail.com>
2018-05-28 01:57:40 +0700

    hotfix: unversioned file for new view

---
+++ Lukin.Nsudotnet.TicTacToe/TicTacToe/Views/EndGameView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace TicTacToe.Views
+{
+    public partial class EndGameView : UserControl
+    {
+        public EndGameView()
+        {
+            InitializeComponent();
+        }
+    }
+}
